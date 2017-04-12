7e1f2f24c82918870311190ea7a48a749db49566
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2017-04-12 12:13:26 +1000

    My Blog Application, first commit.

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+# Register your models here.
+
+# Super User
+# username: helini87
+# password: helloworld87
+
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
